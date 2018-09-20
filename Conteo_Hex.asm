4ba967f3c5bd9467aac077fb50ff0c70f36b9907
Tapia Guerrero Esteban <43421003+ZTNL001@users.noreply.github.com>
2018-09-20 08:14:29 -0500

    Conteo Hexadecimal
    *Conteo de 0x01 a 0x08 en PuertoB

---
+++ Conteo_Hex.asm
@@ -0,0 +1,50 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;EstebanTapia FernandoVargas										;
+;Envio de caracteres en Hexadecimal									;
+;Microcontroladores											;
+;V0 11 de Septiembre de 2018	Inicio de programacion							;
+;V1 12 de Septiembre de 2018	Solucion en seleccion de bancos						;
+;				Correccion en la instruccion MOVLW, anteriormente escrita como MOVL	;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+LIST P=16F887
+#INCLUDE P16F887.INC
+
+__CONFIG _CONFIG1, _WDT_OFF & _CP_OFF  & _BOR_OFF & _LVP_OFF & _XT_OSC & _MCLRE_ON & _CPD_OFF & _PWRTE_OFF
+/*__CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_ON & _FCMEN_ON & _LVP_OFF*/
+
+	ORG	0X00	; inico de programa en flash en rom
+	GOTO 	INICIO
+	ORG 	0X05
+
+INICIO  BSF   STATUS,RP0;Colocarse en banco1
+	MOVLW 0XFF      ;PuertoA como entrada
+	MOVWF TRISA     ;MOVER A registro TRISA el valor de 0xFF
+	MOVLW 0X00	;PuertoB como salida
+	MOVWF TRISB	;Mover a registro TRISB el valor de 0x00
+	BSF STATUS,RP1  ;Cambio a banco 1
+	MOVWF ANSEL	;Desactivar A/D
+	MOVWF ANSELH
+	BCF STATUS,RP1	;Colocarse en banco 0
+	BCF STATUS,RP0	;
+	CLRF PORTB	;Limpieza de PuertoA y PuertoB
+	CLRF PORTA
+
+MAIN
+	MOVLW 0x01
+	MOVWF PORTB
+	MOVLW 0x02
+	MOVWF PORTB
+	MOVLW 0x03
+	MOVWF PORTB
+	MOVLW 0x04
+	MOVWF PORTB
+	MOVLW 0x05
+	MOVWF PORTB
+	MOVLW 0x06
+	MOVWF PORTB
+	MOVLW 0x07
+	MOVWF PORTB
+	MOVLW 0x08
+	MOVWF PORTB
+	GOTO MAIN
+END
